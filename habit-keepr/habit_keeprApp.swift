364791e876a11782c9f6a24a3138b3380a5a426c
Devan <devandbenz@gmail.com>
2024-12-20 18:58:22 -0600

    Initial Commit

---
+++ habit-keepr/habit_keeprApp.swift
@@ -0,0 +1,17 @@
+//
+//  habit_keeprApp.swift
+//  habit-keepr
+//
+//  Created by Devan on 12/20/24.
+//
+
+import SwiftUI
+
+@main
+struct habit_keeprApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
